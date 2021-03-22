bfd17badfb639d07459b9eb9db4b1a3f8bdf6971
Vlozista <alejandrocruzgg@hotmail.com>
2021-03-22 14:17:54 -0600

    octaba subida

--- ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -19,13 +19,39 @@ public class ConceptosBasicos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Automovil miBocho= new Automovil();
+        Automovil miBocho = new Automovil();
         miBocho.setMarca("VW");
         miBocho.setSubMarca("Sedan");
         miBocho.setModelo(1970);
         miBocho.setColor(Color.blue);
         
         System.out.println(miBocho);
+        
+        Automovil miMustang = new Automovil();
+        miMustang.setMarca("Ford");
+        miMustang.setSubMarca("Mustan");
+        miMustang.setModelo(2010);
+        miMustang.setColor(Color.yellow);
+                
+        System.out.println(miMustang);       
+                
+        Automovil miAcura = new Automovil();
+        miAcura.setMarca("Acura");
+        miAcura.setSubMarca("NSX");
+        miAcura.setModelo(2013);
+        miAcura.setColor(Color.gray);
+        
+        System.out.println(miAcura);
+        
+        //Tarea programar los obtejos miAcura y miMustang
+        //temas del libro
+        //mecanismos de abstraccion
+        //control de acceso a una clase
+        //metodos de acceso
+                
     }
+
+   
+        
     
 }
